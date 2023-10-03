20df6f9d19fc2f43ac0cc0153c8916074e4c94a3
Bert Rowett <bertrowett@googlemail.com>
2023-10-03 15:00:42 +0100

    Modify getAll method in BookController to hide additional fields

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -10,7 +10,7 @@ class BookController extends Controller
     public function getAll()
     {
         return response()->json([
-            'data' => Book::with('genre:id,name')->get()->makeHidden(['genre_id', 'created_at', 'updated_at']),
+            'data' => Book::with('genre:id,name')->get()->makeHidden(['genre_id', 'created_at', 'updated_at', 'blurb', 'page_count', 'year', 'review_id', 'claimed_by_email', 'claimed_by_name']),
             'message' => 'Books successfully retrieved'
         ]);
     }
