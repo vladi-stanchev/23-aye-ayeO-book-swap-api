9cc095623f7ddbfc96e164a1820ad57ac5588f27
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-10-03 14:13:52 +0100

    update the books table

---
+++ database/migrations/2023_10_03_130446_update_books_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('claimed_by_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->dropColumn('claimed_by_email');
+        });
+    }
+};
