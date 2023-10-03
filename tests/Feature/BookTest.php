61a5d5b2649951ce386eee7578fbb26b7e7b90f6
Vladi <vsstanchev@gmail.com>
2023-10-03 11:55:53 +0100

    Add PHPUnit test

--- tests/Feature/BookTest.php
+++ tests/Feature/BookTest.php
@@ -30,18 +30,85 @@ class BookTest extends TestCase
                     ])
                     ->has('data', 2, function (AssertableJson $json) {
                         $json->hasAll(['id', 'title', 'author', 'image', 'genre'])
-                            ->whereAllType([
-                                'id' => 'integer',
-                                'title' => 'string',
-                                'author' => 'string',
-                                'image' => 'string',
-                            ])
+                            ->whereAllType(
+                                [
+                                    'id' => 'integer',
+                                    'title' => 'string',
+                                    'author' => 'string',
+                                    'image' => 'string',
+                                ]
+                            )
                             ->has('genre', function (AssertableJson $json) {
                                 $json->hasAll(['id', 'name'])
                                     ->whereAllType([
                                         'id' => 'integer',
                                         'name' => 'string'
                                     ]);
+                            });
+                    });
+            });
+    }
+
+    public function test_get_single_books_success(): void
+    {
+        Book::factory()->create();
+
+        $response = $this->getJson('/api/books/{id}');
+
+        $response->assertStatus(200)
+            ->assertJson(function (AssertableJson $json) {
+
+                $json->hasAll(['message', 'data'])
+                    ->whereAllType([
+                        'message' => 'string'
+                    ])
+                    ->has('data', function (AssertableJson $json) {
+                        $json->hasAll(
+                            [
+                                'id',
+                                'title',
+                                'author',
+                                'blurb',
+                                'claimed_by_name',
+                                'image',
+                                'page_count',
+                                'year',
+                                'genre',
+                                'reviews'
+                            ]
+                        )
+                            ->whereAllType(
+                                [
+                                    'id' => 'integer',
+                                    'title' => 'string',
+                                    'author' => 'string',
+                                    'blurb' => 'string',
+                                    'claimed_by_name' => 'string',
+                                    'image' => 'string',
+                                    'page_count' => 'integer',
+                                    'year' => 'integer'
+
+                                ]
+                            )
+                            ->has('genre', function (AssertableJson $json) {
+                                $json->hasAll(['id', 'name'])
+                                    ->whereAllType(
+                                        [
+                                            'id' => 'integer',
+                                            'name' => 'string'
+                                        ]
+                                    );
+                            })
+                            ->has('reviews', function (AssertableJson $json) {
+                                $json->hasAll(['id', 'name', 'rating', 'review'])
+                                    ->whereAllType(
+                                        [
+                                            'id' => 'integer',
+                                            'name' => 'string',
+                                            'rating' => 'integer',
+                                            'review' => 'string'
+                                        ]
+                                    );
                             });
                     });
             });
