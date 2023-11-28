05354082a7d1a04d97aeb33755e628348798c119
Zdravko Koev <zdravko.j.koev@gmail.com>
2023-11-28 12:15:54 +0000

    Change max review and blurb field length

---
+++ database/migrations/2023_11_28_115727_modify_review_length_in_reviews.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->string('review', 10000)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('reviews', function (Blueprint $table) {
+            $table->string('review', 255)->change();
+        });
+    }
+};
---
+++ database/migrations/2023_11_28_121131_modify_blurb_length_in_books.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('blurb', 10000)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('books', function (Blueprint $table) {
+            $table->string('blurb', 255)->nullable()->change();
+        });
+    }
+};
